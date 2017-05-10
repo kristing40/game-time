7f96d0f559b05be2722f3f03868b898ed5d717a6
Ciara Bujanos <c.buj22@gmail.com>
2017-05-10 14:52:50 -0600

    incomplete square test

---
+++ test/square-test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const square = require('../lib/Square.js');
+
+describe('Square', () => {
+  context('with default attributes', () => {
+    const square = new Square(x, y, width, height, color, image)
+  })
+})
